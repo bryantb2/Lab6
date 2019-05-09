79286d61d211452802c97f9db614abe41b88015b
bryantb2 <bryantb2@my.lanecc.edu>
2019-05-09 14:04:24 -0700

    finished stringhash class and working on testing

--- HashProject/HashTable.cs/StringHash.cs
+++ HashProject/HashTable.cs/StringHash.cs
@@ -11,7 +11,8 @@ namespace HashTableLibrary
         //Class Fields
         const string deleted = "--deleteKeyActivated--";
         const string empty = null;
-
+        bool defaultSize = false;
+        bool definedSize = false;
         int size;
         int largestAddIndex;
         string[] hashArray;
@@ -23,7 +24,8 @@ namespace HashTableLibrary
         {
             hashArray = new string[11];
             this.size = 11;
-            NullifyHashTable();
+            defaultSize = true;
+            NullifyNewHashTable();
         }
 
         //Defined Constructor
@@ -31,7 +33,8 @@ namespace HashTableLibrary
         {
             hashArray = new string[size];
             this.size = size;
-            NullifyHashTable();
+            definedSize = true;
+            NullifyNewHashTable();
         }
 
         //Properties
@@ -114,24 +117,63 @@ namespace HashTableLibrary
 
         public bool RemoveItem(string item)
         {
-            return true;
+            //hash item and use returned index to determine if the value exists in its expected spot
+              //if its there: set the value to const deleted
+                 //return true
+              //else
+                //iterate through the array to find value
+                    //return false if not found
+                //if found, set to deleted and return true
+            int index = Hash(item);
+            if (hashArray[index] == item)
+            {
+                hashArray[index] = deleted;
+                return true;
+            }
+            else
+            {
+                int runningIndex = index;
+                bool isWrapped = false;
+                while (hashArray[runningIndex] != item)
+                {
+                    if (runningIndex == this.size - 1)
+                    {
+                        runningIndex = 0;
+                        isWrapped = true;
+                    }
+                    else if ((runningIndex - 1) == index && isWrapped == true && hashArray[runningIndex] != item) //meaning that it has wrapped and did not break out of loop
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        runningIndex++;
+                    }
+                }
+                hashArray[runningIndex] = deleted;
+                return true;
+                //if false was not returned within the while loop,
+                //then it means that the current runningIndex contains the desired value
+            }
         }
 
-        //Private Methods
+        //PRIVATE METHODS
         private int Hash(string item)
         {
-            //create a running total
-            //use for loop to access the substring of the string
-            //convert the substring to an int (results in ASCII value)
-            //raise the substring to the power of its position
-            //return modulus of the running total
-            int runningTotal = 0;
-            for (int i = 0; i < item.Length; i++)
-            {
-                int ASCIIValue = (int)item[i];
-                runningTotal += (int)(Math.Pow(ASCIIValue, (i + 1)));
-            }
-            return (runningTotal%this.size);
+        //create a running total
+        //use for loop to access the substring of the string
+        //convert the substring to an int (results in ASCII value)
+        //raise the substring to the power of its position
+        //return modulus of the running total
+        int runningTotal = 0;
+        for (int i = 0; i < item.Length; i++)
+        {
+                //int ASCIIValue = (int)item[i];
+                //runningTotal += (int)(Math.Pow(ASCIIValue, (i + 1)));
+                runningTotal *= i;
+                runningTotal += item[i];
+        }
+        return (Math.Abs(runningTotal%this.size));
         }
 
         private int FindNextFreeSpace(int index)
@@ -143,15 +185,16 @@ namespace HashTableLibrary
             else
             {
                 int runningIndex = index;
-                while(hashArray[runningIndex] != deleted || hashArray[runningIndex] != empty || runningIndex != index)
+                bool isWrapped = false;
+                while (hashArray[runningIndex] != deleted || hashArray[runningIndex] != empty || runningIndex != index)
                 {
                     if(runningIndex == this.size - 1)
                     {
                         runningIndex = 0;
                     }
-                    else if((runningIndex - 1)==index && hashArray[runningIndex] != deleted || hashArray[runningIndex] != empty) //meaning that it has wrapped
+                    else if((runningIndex - 1)==index && isWrapped==true && hashArray[runningIndex] != deleted || hashArray[runningIndex] != empty) //meaning that it has wrapped
                     {
-                        return -1;
+                        return -1; //means there is no available space
                     }
                     else
                     {
@@ -181,24 +224,62 @@ namespace HashTableLibrary
 
         private void DoubleHashTable()
         {
+            //find what value in the sizes array that the current hash size is closest to
+                //use for loop to iterate through array, subtract its value from the current size
+                //if the subtracted size is smaller than the current smallest size, add index to variable
+            //make a new hashtable array, with the size found in the array
+            //track old hashtable size
+            //iterate through old hashtable and perform a modulus operator on all non-empty elements
+            int primeNumberIndex = 0;
+            if (definedSize == true)
+            {
+                for (int i = 0; i < primeSizeArray.Length; i++)
+                {
+                    if (this.size * 2 < primeSizeArray[i])
+                    {
+                        //finds first array size value that is at least two times the current size
+                        primeNumberIndex = i;
+                        break;
+                    }
+                }
+            }
+            else if(defaultSize == true)
+            {
+                for (int i = 0; i < primeSizeArray.Length; i++)
+                {
+                    if(this.size == primeSizeArray[i])
+                    {
+                        //finds the size that the array is currently at and goes to the next double size
+                        primeNumberIndex = i+1;
+                        break;
+                    }
+                }
+            }
+            string[] oldHash = hashArray;
+            int oldHashSize = this.size;
+            string[] doubledHashTable = new string[primeSizeArray[primeNumberIndex]];
+            this.hashArray = doubledHashTable;
+            this.size = primeSizeArray[primeNumberIndex];
+
+            for (int i = 0; i < oldHashSize; i++)
+            {
+                if(oldHash[i] != null)
+                {
+                    int index = Hash(oldHash[i]); //hashes into the new array with the new size
+                    index = FindNextFreeSpace(index); //deals with a potential collisio
+                    hashArray[index] = oldHash[i];
+                }
+               
+            }
 
         }
 
-        private void NullifyHashTable()
+        private void NullifyNewHashTable()
         {
             for (int i = 0; i < this.size; i++)
             {
                 hashArray[i] = empty;
             }
         }
-
-        private void NullifyHashTable(int startingPoint)
-        {
-            for (int i = 0; i < this.size; i++)
-            {
-                hashArray[i] = empty;
-            }
-        }
-
     }
 }
---
+++ HashProject/UnitTesting/StringHashTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HashTableLibrary;
+using NUnit.Framework;
+
+namespace UnitTesting
+{
+    [TestFixture]
+    public class StringHashTest
+    {
+        //Class Fields
+        StringHash definedHashTable;
+        StringHash defaultHashTable;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            definedHashTable = new StringHash(17);
+            defaultHashTable = new StringHash();
+        }
+
+        [Test]
+        public void SizePropertyTest()
+        {
+            Assert.AreEqual(17, definedHashTable.Size);
+            Assert.AreEqual(11, defaultHashTable.Size);
+        }
+
+        [Test]
+        public void AddItemTest()
+        {
+            try
+            {
+                defaultHashTable.AddItem("");
+                Assert.Fail("AddItem did not throw an exception");
+            }
+            catch(ArgumentException)
+            {
+                Assert.Pass("The AddItem method threw an exception!");
+            }
+        }
+
+        [Test]
+        public void AddandFindTest()
+        {
+            defaultHashTable.AddItem("obi-wan");
+            defaultHashTable.AddItem("anakin");
+            defaultHashTable.AddItem("grevious");
+            defaultHashTable.AddItem("vader");
+            Assert.AreEqual(true, defaultHashTable.FindItem("vader"));
+            Assert.AreEqual(true, defaultHashTable.FindItem("grevious"));
+            Assert.AreEqual(false, defaultHashTable.FindItem(""));
+            Assert.AreEqual(false, defaultHashTable.FindItem("rader"));
+        }
+
+        [Test]
+        public void DoubleHashTableTest()
+        {
+            int originalSize = defaultHashTable.Size;
+            defaultHashTable.AddItem("obi-wan");
+            defaultHashTable.AddItem("anakin");
+            defaultHashTable.AddItem("grevious");
+            defaultHashTable.AddItem("vader");
+            defaultHashTable.AddItem("abc");
+            defaultHashTable.AddItem("123"); //should double here
+            defaultHashTable.AddItem("890");
+            Assert.AreEqual(originalSize * 2, defaultHashTable.Size);
+        }
+
+
+
+
+
+    }
+}
