3299eb719ed20ff6ace51d960c23aad1a6918ac2
bryantb2 <bryantb2@my.lanecc.edu>
2019-05-08 21:43:44 -0700

    added private and public methods for the StringHash class

--- HashProject/HashTable.cs/StringHash.cs
+++ HashProject/HashTable.cs/StringHash.cs
@@ -48,15 +48,68 @@ namespace HashTableLibrary
         public void AddItem(string item)
         {
             //if item is not empty
-            //call is half full
-                //if true, double the hash table
+                //call is half full
+            //if true, double the hash table
             //call hash method, use return value as index for adding to hashArray
             //record largest add at index if the returned hash value is larger than the current
+            if (item != "")
+            {
+                if(IsHalfFull() == true)
+                {
+                    DoubleHashTable();
+                }
+                else
+                {
+                    int index = Hash(item);
+                    index = FindNextFreeSpace(index);
+                    hashArray[index] = item;
+                    if (index > largestAddIndex)
+                        largestAddIndex = index;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("string parameter must not be empty");
+            }
         }
 
         public bool FindItem(string item)
         {
-            return true;
+            //hash the item and return the index
+                //if the indexed element does not equal item
+                    //iterate through the hasharray until the value is found
+                        //return true if found
+                    //if it loops back to the original index
+                        //return false
+            int index = Hash(item);
+            if(hashArray[index]==item)
+            {
+                return true;
+            }
+            else
+            {
+                int runningIndex = index;
+                bool isWrapped = false;
+                while (hashArray[runningIndex] != item)
+                {
+                    if (runningIndex == this.size - 1)
+                    {
+                        runningIndex = 0;
+                        isWrapped = true;
+                    }
+                    else if ((runningIndex - 1) == index && isWrapped==true && hashArray[runningIndex] != item) //meaning that it has wrapped
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        runningIndex++;
+                    }
+                }
+                return true; 
+                //if false was not returned within the while loop,
+                //then it means that the current runningIndex contains the desired value
+            }
         }
 
         public bool RemoveItem(string item)
@@ -65,9 +118,48 @@ namespace HashTableLibrary
         }
 
         //Private Methods
-        private int Hash()
+        private int Hash(string item)
         {
-            return 1;
+            //create a running total
+            //use for loop to access the substring of the string
+            //convert the substring to an int (results in ASCII value)
+            //raise the substring to the power of its position
+            //return modulus of the running total
+            int runningTotal = 0;
+            for (int i = 0; i < item.Length; i++)
+            {
+                int ASCIIValue = (int)item[i];
+                runningTotal += (int)(Math.Pow(ASCIIValue, (i + 1)));
+            }
+            return (runningTotal%this.size);
+        }
+
+        private int FindNextFreeSpace(int index)
+        {
+            if(hashArray[index] == deleted || hashArray[index] == empty)
+            {
+                return index;
+            }
+            else
+            {
+                int runningIndex = index;
+                while(hashArray[runningIndex] != deleted || hashArray[runningIndex] != empty || runningIndex != index)
+                {
+                    if(runningIndex == this.size - 1)
+                    {
+                        runningIndex = 0;
+                    }
+                    else if((runningIndex - 1)==index && hashArray[runningIndex] != deleted || hashArray[runningIndex] != empty) //meaning that it has wrapped
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        runningIndex++;
+                    }
+                }
+                return runningIndex;
+            }
         }
 
         private bool IsHalfFull()
